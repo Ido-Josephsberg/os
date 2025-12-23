289f88eed6be232b6e36fbaada20512dbace239a
noa-keter <noaketer123@gmail.com>
2025-12-24 01:15:36 +0200

    [os][hw2] execuye_job bugfix in calling the function

--- hw2_318875770_322641135/threads.c
+++ hw2_318875770_322641135/threads.c
@@ -96,7 +96,7 @@ static void* thread_routine(void* arg) {
         if (shared_jobs_queue.log_enabled)
             write_into_log_file(job_to_execute->job_line, thread_index, 1, 0, 0);
         // Execute the fetched job
-        execute_job(job_to_execute);
+        execute_job(job_to_execute->job_cmds);
         // Get end time after job execution for logging purposes
         end_time_ms = get_elapsed_time_ms();
         turnaround_time_ms = end_time_ms - job_to_execute->time_after_reading_line_ms;
